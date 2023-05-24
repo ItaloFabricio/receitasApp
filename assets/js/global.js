e6d968e6a02e88431e8fe7055ffb4c919a803bbd
ítalo Fabrício <italofps65@gmail.com>
2023-05-24 00:17:40 -0300

    feat: navegando nas tabs por clique

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,3 +5,17 @@
  */
 
 "use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NoddeList} $elements Nodelist
+ * @param {String} $elements Event type string
+ * @param {Function} $elements Callback function
+ * 
+ */
+
+window.addEventOnElements = ($elements, everType, callback) => {
+    for(const $element of $elements){
+        $element.addEventListener(everType, callback);
+    }
+}
--- assets/js/home.js
+++ assets/js/home.js
@@ -37,5 +37,19 @@ $searchField.addEventListener('keydown', e => {
 const /* {NodeList} */ $tabBtns = document.querySelectorAll('[data-tab-btn]');
 const /* {NodeList} */ $tabPanels = document.querySelectorAll('[data-tab-panel]');
 
-let /* {NodeList} */ [$lastActivePanel] = $tabPanels;
-let /* {NodeList} */ [$lastActiveBtn] = $tabBtns;
+let /* {NodeElement} */ [$lastActiveTabPanel] = $tabPanels;
+let /* {NodeElement} */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, 'click', function (){
+    $lastActiveTabPanel.setAttribute('hidden', "");
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabBtn.setAttribute('tabindex', -1);
+
+    const /* {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute('aria-controls')}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute('aria-selected', true);
+    this.setAttribute('tabindex', 0);
+
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+});
